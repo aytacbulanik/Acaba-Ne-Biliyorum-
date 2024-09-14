9e2fbec702f8d25eecb5dcd504cde455c6025f96
Aytac Bulanik <aytacbulanik@hotmail.com>
2024-09-14 09:17:27 +0300

    components was connect viewcontroller

--- Acaba Ne Biliyorum ?/ViewController.swift
+++ Acaba Ne Biliyorum ?/ViewController.swift
@@ -9,12 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var trueButtonOut: UIButton!
+    @IBOutlet weak var falseButtonOut: UIButton!
+    @IBOutlet weak var progressViewOut: UIProgressView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
        
     }
 
-
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+    }
+    
 }
 
