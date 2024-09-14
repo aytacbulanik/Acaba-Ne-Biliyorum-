e6cf2f3f0b5d5a96b48ecbc924bf814410a019ec
Aytac Bulanik <aytacbulanik@hotmail.com>
2024-09-14 15:40:31 +0300

    array remove and struct added this app

---
+++ Acaba Ne Biliyorum ?/Question.swift
@@ -0,0 +1,13 @@
+//
+//  Question.swift
+//  Acaba Ne Biliyorum ?
+//
+//  Created by Aytaç Bulanık on 14.09.2024.
+//
+
+import Foundation
+
+struct Question {
+    let q : String
+    let a : String
+}
